e2b13d07b0e5710d4173a20d59d36ffec148983e
AleksGin <alexanderginin@icloud.com>
2025-01-04 23:11:28 +0300

    minor change: rename attribute "element" to "message"

--- LocalNetwork/data.py
+++ LocalNetwork/data.py
@@ -1,4 +1,4 @@
 class Data:
-    def __init__(self, element: str, server_number: int) -> None:
-        self.element = element
+    def __init__(self, message: str, server_number: int) -> None:
+        self.message = message
         self.server_number = server_number
