7b9f5918a07a437b96de48858237894641f22876
AleksGin <alexanderginin@icloud.com>
2025-01-04 21:53:54 +0300

    create Data class for LocalNetwork folder

---
+++ LocalNetwork/data.py
@@ -0,0 +1,10 @@
+class Data:
+    def __init__(self, element: str, server_number: int) -> None:
+        self.element = element
+        self.server_number = server_number
+        
+        
+        
+        
+        
+    
