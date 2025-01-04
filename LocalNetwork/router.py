e8242d79961bf496074f889395ac8050fe7aa6bd
AleksGin <alexanderginin@icloud.com>
2025-01-04 23:11:39 +0300

    created Router class

---
+++ LocalNetwork/router.py
@@ -0,0 +1,40 @@
+from typing import List
+
+from data import Data
+from server import Server
+
+
+class Router:
+    def __init__(self) -> None:
+        self.buffer: List[Data] = []
+        self.servers: dict[int, Server] = {}
+
+    def link(self, server: Server) -> None:
+        if server.get_ip() in self.servers:
+            raise ValueError(f"Сервер с IP: {server.get_ip()} уже привязан к роутеру")
+        server.router = self
+        self.servers[server.get_ip()] = server
+
+    def unlink(self, server: Server) -> None:
+        if server.get_ip() not in self.servers:
+            raise KeyError(f"Сервер с IP: {server.get_ip()} не привязан к роутеру")
+        del self.servers[server.get_ip()]
+        server.router = None
+
+    def send_data(self) -> None:
+        for packet in self.buffer:
+            destination_server = self.servers.get(packet.server_number)
+            if destination_server is None:
+                raise ValueError(f"Сервер с IP: {packet.server_number} не привязан к роутеру")
+            destination_server.buffer.append(packet)
+        self.buffer.clear()
+
+
+router = Router()
+server_1 = Server()
+server_2 = Server()
+router.link(server_1)
+router.link(server_2)
+server_1.send_data(Data("Hello from Server 1 to Server 2", server_2.get_ip()))
+server_1.send_data(Data("Hello again from Server 1 to Server 2", server_2.get_ip()))
+router.send_data()
